0fb8733df86987c06096931b00b716fc1956a711
benttruong <92412837+bentruong203@users.noreply.github.com>
2023-05-03 12:50:43 -0700

    5-3 push

--- turtle/turtleBen.c
+++ turtle/turtleBen.c
@@ -1,45 +1,4 @@
 
-/*
-    FPToolkit.c : A simple set of graphical tools.
-    FPToolkitDemo.c 
-    Copyright (C) 2018  Ely
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License (version 3)
-    as published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-
-
-
-
-/*
-
-This code assumes an underlying X11 environment.
-
-       Most freshly installed unbuntu environments do not have
-       the X11 developr stuff they'll need to do graphics.
-       To download X11 developer stuff, connect to the internet and 
-       issue the following two commands.  Each will ask for your password
-       and each will take a few minutes.  At some point it might even
-       look like nothing is happening....be patient :
-       
-sudo  apt-get  install  libx11-dev     
-
-sudo  apt-get  install  xorg-dev
-
-*/
-
-
-
 
 /*
 If this file and the file, FPToolkit.c, are in the
@@ -56,36 +15,37 @@ note : the 'X' in -X11 is capitalized
 #include  "string_builder.c"
 
 int strleng = 2000000;
-double angle = 60.0;
+double angle = 30.0;
 double angle_current = 0;
-double length = 25.0;
+double length = 1.0;
 double x_value, y_value;
-double x_current = 300.0;
-double y_current = 300.0;
+double x_current = 100.0;
+double y_current = 10.0;
 void rotate(double a);
 void step();
 void values_reset();
 
 void positive_rot() { angle_current += angle; };
 void negative_rot() { angle_current -= angle; };
+void draw(char str []);
+double auto_placer (char str []);
+
+int canvas_size = 800;
+int margin = 50;
+
 
 int main()
 {
    char str [strleng];
-   string_builder(str);
+   // string_builder(str);
    // if you want to use external file, comment out the line above and uncomment the line below:
-//   scanf("%s", str); 
+   scanf("%s", str); 
 
    int    swidth, sheight ;
-   double lowleftx, lowlefty, width, height ;
-   double x[10], y[10] ;
-   double numxy ;
-   double a[20], b[20] ;
-   double numab ;
 
    
    // must do this before you do 'almost' any other graphical tasks 
-   swidth = 400 ;  sheight = 600 ;
+   swidth = canvas_size ;  sheight = canvas_size ;
    G_init_graphics (swidth,sheight) ;  // interactive graphics
 
    
@@ -103,16 +63,11 @@ int main()
 
    G_rgb(1,0,0) ;
    
-   int i = 0;
- 
-   // char str [strleng];
-   while (str[i] != '\0' && i < strleng) {
-   	if (str[i] >= 'A' && str[i] <= 'Z') step();
-	else if (str[i] == '+') positive_rot();
-        else negative_rot();
-	++i;
-   }	
-   
+   length = auto_placer(str); 
+   x_current = 100;
+   y_current = 50;
+   printf("x: %f, y: %f, scale: %f", x_current, y_current, length);
+   draw(str); 
 
 
    int key ;   
@@ -120,6 +75,68 @@ int main()
 
    //   G_save_image_to_file("demo.xwd") ;
    G_save_to_bmp_file("demo.bmp") ;
+}
+
+double auto_placer(char str []) {
+   double top_y = 0.0;
+   double bot_y = 0.0;
+   double right_x = 0.0;
+   double left_x = 0.0;
+
+   int i = 0; 
+   while (str[i] != '\0' && i < strleng) {
+   
+   	if (str[i] >= 'A' && str[i] <= 'Z') {
+   	   values_reset();
+   	   rotate(angle_current);
+   	   x_current += x_value;
+   	   y_current += y_value;
+   	   if (x_current >= right_x) right_x = x_current; 
+   	   if (x_current < left_x)   left_x = x_current; 
+   	   if (y_current >= top_y)   top_y = y_current; 
+   	   if (y_current < bot_y)    bot_y = y_current; 
+	   values_reset();
+   	}
+	else if (str[i] == '+') positive_rot();
+        else negative_rot();
+	++i;
+   }
+   
+   // finding the height of image:
+   double image_height = top_y - bot_y;
+   // finding the width of image: 
+   double image_width = right_x - left_x;
+   
+   // the scale:
+   double scale;
+   if (image_height >= image_width) 
+      // scale = image_height/(canvas_size-(margin*2));
+      scale = (canvas_size-(margin*2))/image_height;
+   else
+      // scale = image_width/(canvas_size-(margin*2));
+      scale = (canvas_size-(margin*2))/image_width;
+       
+   // finding the center of mass
+   double x_center = (right_x + left_x)/2;
+   double y_center = (top_y + bot_y)/2;
+   
+   // setting starting point based on scale and center of mass:
+   x_current = (canvas_size/2) - x_center;
+   y_current = (canvas_size/2) - y_center;
+
+   return scale;
+    
+}
+
+void draw (char str[]) {
+   int i = 0; 
+   while (str[i] != '\0' && i < strleng) {
+   	if (str[i] >= 'A' && str[i] <= 'Z') step();
+	else if (str[i] == '+') positive_rot();
+        else negative_rot();
+	++i;
+   }	
+
 }
 
 void rotate(double a) {
