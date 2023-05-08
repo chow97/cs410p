83d4a0a800f30b5b3d967b069b0b0f2ccfd16778
chow97 <kahoo.chow@gmail.com>
2023-05-08 11:58:19 -0700

    adding ifs

---
+++ ifs/ifsBen.c
@@ -0,0 +1,13 @@
+
+/*
+If this file and the file, FPToolkit.c, are in the
+same directory that you are trying to compile in,  
+do the following :
+
+cc  FPToolkitDemoB.c   -lm  -lX11
+
+note : the 'X' in -X11 is capitalized
+*/
+
+
+#include  "FPToolkit.c"
