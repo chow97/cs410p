e9a6d6329c9e6bf2fd6ee2b91fe9d9b71a1f9e3b
chow97 <kahoo.chow@gmail.com>
2023-05-17 16:15:07 -0700

    done PSU initial.c

--- ifs/initials.c
+++ ifs/initials.c
@@ -19,43 +19,69 @@ void translate(double tfx, double tfy){
     y += tfy;
 }
 void rotate(double angle) {
-   x = cos(angle*M_PI/180);
-   y = sin(angle*M_PI/180);
+    double t = angle * (M_PI/180);
+    double temp;
+
+    temp = (x * cos(t)) - (y * sin(t));
+    y = (y * cos(t)) + (x * sin(t));
+    x = temp;
 }
 // Babies
+//P
 void rule1(){
-    scale(1.0/3.0, 1.0/3.0);
+    scale(5.0/13.0, 1.0/7.0);
+    rotate(90.0);
+    translate(2.0/13.0,1.0/13.0);
 }
 void rule2(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(1.0/3.0, 0);
+    scale(2.0/13.0, 1.0/7.0);
+    translate(2.0/13.0,5.0/13.0);
 }
 void rule3(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(2.0/3.0, 0);
+    scale(2.0/13.0, 1.0/7.0);
+    translate(2.0/13.0,3.0/13.0);
 }
 void rule4(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(0, 1.0/3.0);
+    scale(1.0/13.0, 1.0/7.0);
+    translate(3.0/13.0,4.0/13.0);
 }
+//S
 void rule5(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(0, 2.0/3.0);
+    scale(3.0/13.0, 1.0/7.0);
+    translate(5.0/13.0,5.0/13.0);
 }
 void rule6(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(1.0/3.0, 2.0/3.0);
+    scale(3.0/13.0, 1.0/7.0);
+    translate(5.0/13.0,3.0/13.0);
 }
 void rule7(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(2.0/3.0, 2.0/3.0);
+    scale(3.0/13.0, 1.0/7.0);
+    translate(5.0/13.0,1.0/13.0);
 }
 void rule8(){
-    scale(1.0/3.0, 1.0/3.0);
-    translate(2.0/3.0, 1.0/3.0);
+    scale(1.0/13.0, 1.0/7.0);
+    translate(5.0/13.0,4.0/13.0);
+}
+void rule9(){
+    scale(1.0/13.0, 1.0/7.0);
+    translate(7.0/13.0,2.0/13.0);
+}
+//U
+void rule10(){
+    scale(5.0/13.0, 1.0/7.0);
+    rotate(90.0);
+    translate(10.0/13.0,1.0/13.0);
+}
+void rule11(){
+    scale(5.0/13.0, 1.0/7.0);
+    rotate(90.0);
+    translate(12.0/13.0,1.0/13.0);
+}
+void rule12(){
+    scale(1.0/13.0, 1.0/7.0);
+    translate(10.0/13.0, 1.0/13.0);
 }
 
-void do_nothing() {return;};
 
 int main(){
     // canvas heght and width
@@ -71,31 +97,23 @@ int main(){
 
     for(i = 0; i < ITER; ++i){
         n = drand48();
-        /* if(n < 1.0/9.0) { rule1(); }
-        else if(n > 1.0/9.0 && n < 2.0/9.0) { rule2(); }
-        else if(n > 2.0/9.0 && n < 3.0/9.0) { rule3(); }
-        else if(n > 3.0/9.0 && n < 4.0/9.0) { rule4(); }
-        else if(n > 4.0/9.0 && n < 5.0/9.0) { rule5(); }
-        else if(n > 5.0/9.0 && n < 6.0/9.0) { rule6(); }
-        else if(n > 6.0/9.0 && n < 7.0/9.0) { rule7(); }
-        // else if(n > 7.0/9.0 && n < 8.0/9.0) { rule8(); }
-        else { rule8(); }
-        */
-
-        	
-        if (n < 1.0/4.0) { 
-           scale(0.9, 0.9);
-	   rotate(10.0);
-	   translate(x*0.1, y*0.1);	   
-	}
-        // else if (n > 1.0/4.0 && n < 2.0/4.0) do_nothing();
-        // else if(n > 2.0/4.0 && n < 3.0/4.0) do_nothing();
-        // else (n > 3.0/4.0 && n < 4.0/4.0) do_nothing();
-	G_point(x*swidth, y*sheight);
+        if(n < 1.0/12.0) { rule1(); }
+        else if(n > 1.0/12.0 && n < 2.0/12.0) { rule2(); }
+        else if(n > 2.0/12.0 && n < 3.0/12.0) { rule3(); }
+        else if(n > 3.0/12.0 && n < 4.0/12.0) { rule4(); }
+        else if(n > 4.0/12.0 && n < 5.0/12.0) { rule5(); }
+        else if(n > 5.0/12.0 && n < 6.0/12.0) { rule6(); }
+        else if(n > 6.0/12.0 && n < 7.0/12.0) { rule7(); }
+        else if(n > 7.0/12.0 && n < 8.0/12.0) { rule8(); }
+        else if(n > 8.0/12.0 && n < 9.0/12.0) { rule9(); }
+        else if(n > 9.0/12.0 && n < 10.0/12.0) { rule10(); }
+        else if(n > 10.0/12.0 && n < 11.0/12.0) { rule11(); }
+        else { rule12(); }
+        G_point(x*swidth, y*sheight);
     }
 
 
     G_wait_key();
-
+    G_save_to_bmp_file("PSU.bmp");
     return 0;
 }
