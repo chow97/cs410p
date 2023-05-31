b47d8e2302b937750c5f0431a67ec1685cd7bd6f
benttruong <92412837+bentruong203@users.noreply.github.com>
2023-05-31 13:18:34 -0700

    current mandel

--- complex/con-div.c
+++ complex/con-div.c
@@ -1,20 +1,63 @@
 #include <stdio.h>
+#include <complex.h>
+#include "FPToolkit.c"
 
-int main () {
+int Wsize = 800;
 
-    double z = 0;
-    double r;
+void prcmx (char *control, complex c)
+//print complex number
+{
+  double a,b ;
+  a = creal(c) ;
+  b = cimag(c) ;
 
-    printf("Please enter number r: ");
-    scanf("%lf", &r);
-    for (int i = 0; i < 100000; ++i) {
-        z = z*z + r;
-        // printf("z: %lf\n", z);
-        if (z > 1) { printf("Diverge\n"); return 1; }
+  printf(control,a) ;
+  if (b >= 0) {
+    printf("+") ;
+  } else {
+    printf("-") ;
+  }
+  printf(control,fabs(b)) ;  
+  printf("I") ;
+}
+
+int check (complex c) {
+    // return 1 if diverge - return 0 if converge
+    complex z = 0 + 0*I;
+    printf("Entered check function with "); prcmx("%20.16lf",c) ; printf("\n") ;
+    for (int i = 0; i < 100; ++i) {
+        z = z*z + c;
+        if (cabs(z) > 2)
+            { printf ("Diverge\n"); return 1; }
     }
 
     printf ("Converge\n");
+    return 0;
+}
 
+int main () {
+
+    G_init_graphics (Wsize, Wsize) ;  // interactive graphics
+    // clear the screen in a given color
+    G_rgb (0, 0, 0) ; // black screen
+    G_clear () ;
+
+    double unit = 4/800;
+
+    a is the real part, b is the coefficient
+    for (double a = -2.0; a < 2; a = a + unit) {
+        for (double b = -2.0; b < 2; a = b + unit) {
+            if (check(a + b*I) == 1) {
+                G_rgb(0,1,0);
+                G_point ((a+2)*800, (b+2)*800);
+            } else {
+                G_rgb(0,0,1);
+                G_point ((a+2)*800, (b+2)*800);
+            } 
+        }
+    }
+
+    G_wait_key();
     return 0;
 
 }
