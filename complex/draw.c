1ec35889c0c9a3cdb66870fef6103d61b11b55b1
chow97 <kahoo.chow@gmail.com>
2023-06-03 15:05:47 -0700

    update my complex

--- complex/draw.c
+++ complex/draw.c
@@ -30,10 +30,10 @@ int check (complex c) {
     // printf("Entered check function with "); prcmx("%20.16lf",c) ; printf("\n") ;
     for (int i = 0; i < 100; ++i) {
         z = z*z + c;
-        if (cabs(z) > 2) {return 1; }
+        if (cabs(z) > 2) {return 1; } //diverge
     }
 
-    return 0;
+    return 0; //converge
 }
 
 int main () {
